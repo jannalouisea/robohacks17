c9992b8eea5be70fe77667bd80a5881f49403002
Jonathan Adalin <JonathanAdalin@gmail.com>
2017-03-25 21:54:37 -0400

    added 2 ir sensors, switch to arduino uno

--- src/main/main/main.ino
+++ src/main/main/main.ino
@@ -1,17 +1,39 @@
-#include <Wire.h>
-#include <Zumo32U4.h>
+int delayTime = 500;
+int distThresh = 100;
 
-int sensorpin = A0;                 // analog pin used to connect the sharp sensor
-int val = 0;                 // variable to store the values from sensor(initially zero)
+int leftIrPin = A0;
+int rightIrPin = A1;
+
+int leftDist = 0;
+int rightDist = 0;
 
 void setup()
 {
-  Serial.begin(9600);               // starts the serial monitor
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
 }
  
 void loop()
 {
-  val = analogRead(sensorpin);       // reads the value of the sharp sensor
-  Serial.println(val);            // prints the value of the sensor to the serial monitor
-  delay(400);                    // wait for this much time before printing next value
+  readLeft();
+  readRight();
+  if (leftDist > distThresh || rightDist > distThresh){
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW); 
+  }
+  delay(delayTime);
 }
+
+void readLeft(){
+  leftDist = analogRead(leftIrPin);
+  Serial.print("Left: ");
+  Serial.print(leftDist); 
+}
+
+void readRight(){
+  rightDist = analogRead(rightIrPin);
+  Serial.print(" Right: ");
+  Serial.println(rightDist); 
+}
+
